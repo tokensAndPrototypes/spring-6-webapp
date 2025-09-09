9e968a38d11942b7e4cb79f8ecfbd3d5157a8d26
tokensAndPrototypes <patrick.w.warren@gmail.com>
2025-09-09 15:33:30 -0500

    Copied in Initializr generated project

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring6WebappApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring6WebappApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebappApplication.class, args);
+	}
 
 }
--- src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
+++ src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring6WebappApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
